229b8b57d431ed7819297eae5e5ae5cb8152d685
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2018-01-21 14:16:21 +0100

    fixes IMU

--- libclaraquino/modules/imu_lsm9ds1/SparkFunLSM9DS1.cpp
+++ libclaraquino/modules/imu_lsm9ds1/SparkFunLSM9DS1.cpp
@@ -164,24 +164,29 @@ uint16_t LSM9DS1::begin()
 		
 	// To verify communication, we can read from the WHO_AM_I register of
 	// each device. Store those in a variable so we can return them.
-	uint8_t mTest = mReadByte(WHO_AM_I_M);		// Read the gyro WHO_AM_I
-	uint8_t xgTest = xgReadByte(WHO_AM_I_XG);	// Read the accel/mag WHO_AM_I
-	uint16_t whoAmICombined = (xgTest << 8) | mTest;
+	//uint8_t mTest = mReadByte(WHO_AM_I_M);		// Read the gyro WHO_AM_I
+	uint8_t xgTest; 
 	
-	if (whoAmICombined != ((WHO_AM_I_AG_RSP << 8) | WHO_AM_I_M_RSP))
+	for (int i=0;i<3;i++)
+	{
+	xgTest= xgReadByte(WHO_AM_I_XG);	// Read the accel/mag WHO_AM_I
+	if (xgTest==WHO_AM_I_AG_RSP)
+		break;
+	}
+	if (xgTest!=WHO_AM_I_AG_RSP)
 		return 0;
 	
 	// Gyro initialization stuff:
-	initGyro();	// This will "turn on" the gyro. Setting up interrupts, etc.
+	//initGyro();	// This will "turn on" the gyro. Setting up interrupts, etc.
 	
 	// Accelerometer initialization stuff:
 	initAccel(); // "Turn on" all axes of the accel. Set up interrupts, etc.
 	
 	// Magnetometer initialization stuff:
-	initMag(); // "Turn on" all axes of the mag. Set up interrupts, etc.
+	//initMag(); // "Turn on" all axes of the mag. Set up interrupts, etc.
 
 	// Once everything is initialized, return the WHO_AM_I registers we read:
-	return whoAmICombined;
+	return xgTest;
 }
 
 void LSM9DS1::initGyro()
@@ -1059,7 +1064,7 @@ void LSM9DS1::initSPI()
 	// Data is read and written MSb first.
 	// Data is captured on rising edge of clock (CPHA = 0)
 	// Base value of the clock is HIGH (CPOL = 1)
-	spi_settings(5000000,SPI_MSB_FIRST,SPI_MODE0);
+	spi_settings(100000,SPI_MSB_FIRST,SPI_MODE0);
 }
 
 void LSM9DS1::SPIwriteByte(uint8_t csPin, uint8_t subAddress, uint8_t data)
